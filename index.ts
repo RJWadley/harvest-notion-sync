a429957f39614a036b9bd25b0c644b4bbd7a6429
Robbie <robbiethewadley@gmail.com>
2024-04-12 10:30:19 -0600

    axe the date thing

--- index.ts
+++ index.ts
@@ -200,13 +200,6 @@ const check = async () => {
 											content: `${roundedTime} Hours\t`,
 										},
 									},
-									// current date, if desired
-									{
-										type: "equation",
-										equation: {
-											expression: `^{${currentDate?.split("-").join("-")}}`,
-										},
-									},
 								]
 							: [
 									{
